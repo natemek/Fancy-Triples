6ccaa8405b9f8235615d9c6f51982cb228aa5ca5
Natnael Mekonnen <natnaelmek@gmail.com>
2021-03-31 17:17:53 -0400

    created project

---
+++ assign3/assign3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assign3
+//
+//  Created by Natnael Mekonnen on 3/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assign3/assign3/assign3App.swift
@@ -0,0 +1,17 @@
+//
+//  assign3App.swift
+//  assign3
+//
+//  Created by Natnael Mekonnen on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct assign3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
